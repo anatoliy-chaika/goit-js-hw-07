313cd3ec65c3f82622ed740f04b43fc9ced967c9
anatoliy-chaika <110462151+anatoliy-chaika@users.noreply.github.com>
2022-12-15 00:00:30 +0200

    1 task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", onClickImage);
+
+function onClickImage(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const imgSRC = e.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(
+    `
+    <div class="modal">
+       <img src="${imgSRC}" width="800" height="600">
+    </div>
+`
+  );
+
+  instance.show(() => window.addEventListener("keydown", onListner));
+
+  function onListner(evt) {
+    console.log(evt.code);
+    if (evt.code === "Escape") {
+      instance.close();
+      removeKeyboardListener();
+    }
+
+    function removeKeyboardListener() {
+      window.removeEventListener("keydown", onListner);
+    }
+  }
+}
+
+const createMurcupImages = galleryItems
+  .map(({ preview, original, description }) => {
+    const murcup = `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    return murcup;
+  })
+  .join("");
+gallery.insertAdjacentHTML("beforeend", createMurcupImages);
